166905df8651ada90c75d5adb7af4436172add02
j0wiz <jorizivannvillanueva18@gmail.com>
2021-03-06 19:31:39 +0800

    Initial commit

---
+++ carrots.c
@@ -0,0 +1,19 @@
+//Solving for Carrots
+#include <stdio.h>
+
+int main()
+{
+    int n, p, i;
+    char s[100];
+    
+    scanf("%d %d", &n, &p);
+    
+    for (i = 0; i < n; i++)
+    {
+        scanf("%s", s);
+    }
+    
+    printf("%d\n", p);
+
+    return 0;
+}
