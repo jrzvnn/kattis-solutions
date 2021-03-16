b33a14fd0f0e0db9af1a3de2d81dab20d7bda459
j0wiz <jorizivannvillanueva18@gmail.com>
2021-03-16 23:11:54 +0800

    Initial commit

---
+++ areal.c
@@ -0,0 +1,15 @@
+//A Real Challenge
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, out;
+    
+    scanf("%f", &a);
+    out = sqrt(a) * 4;
+    
+    printf("%f", out);
+
+    return 0;
+}
