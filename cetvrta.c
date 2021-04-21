c502cde4eda9b3dc7cd43351f9f667a610be8c79
pwned11 <jorizivannvillanueva18@gmail.com>
2021-04-21 23:01:42 +0800

    Initial commit

---
+++ cetvrta.c
@@ -0,0 +1,42 @@
+//Cetvrta
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x1, y1, x2, y2, x3, y3, x4 = 0, y4 = 0;
+	
+	scanf("%d %d", &x1, &y1);
+	scanf("%d %d", &x2, &y2);
+	scanf("%d %d", &x3, &y3);
+	
+	if (x1 == x2) 
+	{
+		x4 = x3;
+	} 
+	else if (x1 == x3)
+	{
+		x4 = x2;
+	} 
+	else 
+	{
+		x4 = x1;
+	}
+	
+	if (y1 == y2) 
+	{
+		y4 = y3;
+	} 
+	else if (y1 == y3) 
+	{
+		y4 = y2;
+	} 
+	else 
+	{
+		y4 = y1;
+	}
+	
+	printf("%d %d", x4, y4);
+	
+	return 0;
+}
