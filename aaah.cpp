008754a6397286ea8937c72f78855da233d8bcd4
pwned11 <jorizivannvillanueva18@gmail.com>
2021-08-14 23:05:13 +0800

    Initial commit

---
+++ aaah.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << ((s1.size() < s2.size()) ? "no" : "go") << '\n';    
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
