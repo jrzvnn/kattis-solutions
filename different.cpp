27556031cda91de2eb462106b94715b94ecfb9e8
pwned11 <jorizivannvillanueva18@gmail.com>
2021-08-15 22:43:15 +0800

    Initial commit

---
+++ different.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+
+using namespace std;
+
+void solve() {
+    ll a, b;
+    while(cin >> a >> b) {
+        cout << abs(a - b) << '\n';
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
+
